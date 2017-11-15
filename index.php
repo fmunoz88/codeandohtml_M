cfa4edf3c78a769b5ef3b8bfd8b89914957008b1
fmunoz88 <fmunoz88@outlook.com>
2017-11-15 17:35:15 -0300

    Upload Files
    Primer commit del proyectos con todos los archivos e imágenes

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="css/main.css">
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="nav-wrapper container">
+                    <a href="#" class="brand-logo">Codeando HTML</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="sass.html">Sass</a></li>
+                        <li><a href="badges.html">Components</a></li>
+                        <li><a href="collapsible.html">JavaScript</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <br>
+        <section class="grid container">
+            
+            <!-- Page Layout here -->
+            <div class="row">
+                <!-- LEFT SIDE -->
+                <div class="col s12 l9">
+                <!-- Grey navigation panel -->
+                
+                    <!-- Article 1     -->
+                    <div class="col s12">
+                        <div class="card horizontal">
+                            <div class="card-image img-header">
+                                <img src="img/medium/angular.png">
+                            </div>
+                            <div class="card-article card-stacked">
+                                <div class="card-content">
+                                    <p>I am a very simple card. I am good at containing small bits of information.</p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="#">This is a link</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Article 1     -->
+                    <div class="col s12">
+                        <div class="card horizontal">
+                            <div class="card-image img-header">
+                                <img src="img/medium/html5_orange.png">
+                            </div>
+                            <div class="card-article card-stacked">
+                                <div class="card-content">
+                                    <p>I am a very simple card. I am good at containing small bits of information.</p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="#">This is a link</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Article 1     -->
+                    <div class="col s12">
+                        <div class="card horizontal">
+                            <div class="card-image img-header">
+                                <img src="img/medium/php.png">
+                            </div>
+                            <div class="card-article card-stacked">
+                                <div class="card-content">
+                                    <p>I am a very simple card. I am good at containing small bits of information.</p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="#">This is a link</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Article 1     -->
+                    <div class="col s12">
+                        <div class="card horizontal">
+                            <div class="card-image img-header">
+                                <img src="img/medium/python.png">
+                            </div>
+                            <div class="card-article card-stacked">
+                                <div class="card-content">
+                                    <p>I am a very simple card. I am good at containing small bits of information.</p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="#">This is a link</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Article 1     -->
+                    <div class="col s12">
+                        <div class="card horizontal">
+                            <div class="card-image img-header">
+                                <img src="img/medium/mysql.png">
+                            </div>
+                            <div class="card-article card-stacked">
+                                <div class="card-content">
+                                    <p>I am a very simple card. I am good at containing small bits of information.</p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="#">This is a link</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Article 1     -->
+                    <div class="col s12">
+                        <div class="card horizontal">
+                            <div class="card-image img-header">
+                                <img src="img/medium/html_css.png">
+                            </div>
+                            <div class="card-article card-stacked">
+                                <div class="card-content">
+                                    <p>I am a very simple card. I am good at containing small bits of information.</p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="#">This is a link</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                <!-- RIGHT SIDE -->
+                <div class="col s12 l3">
+                    <!-- Teal page content  -->
+                    
+                    <div class="row">
+                    <div class="col s12 ">
+                    <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                    <span class="card-title">Card Title</span>
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                    I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                    <a href="#">This is a link</a>
+                    <a href="#">This is a link</a>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+
+            </div>
+            
+        </section>
+        <footer class="page-footer">
+        <div class="container">
+        <div class="row">
+        <div class="col l6 s12">
+        <h5 class="white-text">Footer Content</h5>
+        <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+        <h5 class="white-text">Links</h5>
+        <ul>
+        <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+        <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+        <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+        <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="footer-copyright">
+        <div class="container">
+        © 2014 Copyright Text
+        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+        </div>
+        </div>
+        </footer>
+        
+        <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+</html>
